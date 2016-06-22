4d31fe33606ab8fa2557562ba1e68d124df743cf
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-22 15:17:56 +1200

    Create ldap.php

---
+++ lib/ldap.php
@@ -0,0 +1,9 @@
+<?php
+/************************************************************
+* FILENAME:    logging.php
+* DESCRIPTION: ldap functions for php
+* 
+* AUTHOR:      Patrick K. Ryon (Slashdoom)
+* LICENSE:     BSD 3-clause (see LICENSE file)
+************************************************************/
+
