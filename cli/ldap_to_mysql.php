b9ce461c695c434c00921467894a5a777c1bafa5
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-22 14:15:16 +1200

    Update ldap_to_mysql.php

--- cli/ldap_to_mysql.php
+++ cli/ldap_to_mysql.php
@@ -1,7 +1,11 @@
 <?php
-  $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+  $root1 = realpath($_SERVER["DOCUMENT_ROOT"]);
+  $root2 = realpath(dirname(__FILE__));
   
-  echo $root;
+  echo "\r\n";
+  echo $root1;
+  echo "\r\n";
+  echo $root2;
   //include_once("../config/config.php");
   
 ?>
