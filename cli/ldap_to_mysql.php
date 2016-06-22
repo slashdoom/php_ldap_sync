aaac636b59dac4a2b1ea6abc2c2e559944eb7819
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-22 14:32:59 +1200

    Update ldap_to_mysql.php

--- cli/ldap_to_mysql.php
+++ cli/ldap_to_mysql.php
@@ -3,6 +3,7 @@
   $root = realpath(dirname(__FILE__));
   
   include_once(realpath($root.'/../config/config.php'));
+  include_once(realpath($root.'/../lib/logger.php'));
   
   echo "\n\r\n\r";
   echo $db_rw_user;
