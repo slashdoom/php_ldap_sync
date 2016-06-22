b850a10fda5c2f9b66981540a2512aa08b02fdde
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-23 08:37:19 +1200

    Update ldap_to_mysql.php

--- cli/ldap_to_mysql.php
+++ cli/ldap_to_mysql.php
@@ -9,7 +9,7 @@
   $logger = new logger(realpath($root.'/../log/_default.log'),'debug');
   
   echo "\n\rStarting sync...";
-  $logger->debug("Starting sync...");
+  $logger->debug("Starting sync...\n\r");
   
   print_r(ldap_get_members($ldap_fqdn,$ldap_port,$ldap_search_user,$ldap_search_pass,"CN=wifi_byod_access,OU=groups,OU=org,DC=ad,DC=slashdoom,DC=lan"));
 
