078c51a426a9a482f61fc723eb9da3d61de442e5
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-23 08:44:34 +1200

    Update ldap.php

--- lib/ldap.php
+++ lib/ldap.php
@@ -75,7 +75,8 @@ function ldap_get_members($ldap_fqdn,$ldap_port,$ldap_user,$ldap_pass,$search_gr
         return "ldap attribute search failed, check query info";
       }
       $member_attr = ldap_get_entries($ldap_conn_stat,$member_result_stat);
-      print_r($member_attr);
+      //print_r($member_attr);
+      echo $member_attr[0][userprincipalname][0];
       // remove count header elements
       //array_shift($member_attr[0]['userprincipalname']);
       //array_shift($member_attr[0]['useraccountcontrol']);
