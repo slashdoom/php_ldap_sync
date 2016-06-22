a304225079c058a845c12aa80994d9da807c80d4
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-23 10:17:24 +1200

    Update ldap_to_mysql.php

--- cli/ldap_to_mysql.php
+++ cli/ldap_to_mysql.php
@@ -2,15 +2,15 @@
 
   $root = realpath(dirname(__FILE__));
   
-  include_once(realpath($root.'/../config/config.php'));
-  include_once(realpath($root.'/../lib/logging.php'));
-  include_once(realpath($root.'/../lib/ldap.php'));
+  require(realpath($root.'/../config/config.php'));
+  require(realpath($root.'/../lib/logging.php'));
+  require(realpath($root.'/../lib/ldap.php'));
   
   $logger = new logger(realpath($root.'/../log/_default.log'),'debug');
   
   echo "\n\rStarting sync...\n\r";
   $logger->debug("Starting sync...");
   
-  print_r(ldap_get_members($ldap_fqdn,$ldap_port,$ldap_search_user,$ldap_search_pass,"CN=wifi_byod_access,OU=groups,OU=org,DC=ad,DC=slashdoom,DC=lan"));
+  //print_r(ldap_get_members($ldap_fqdn,$ldap_port,$ldap_search_user,$ldap_search_pass,"CN=wifi_byod_access,OU=groups,OU=org,DC=ad,DC=slashdoom,DC=lan"));
 
 ?>
