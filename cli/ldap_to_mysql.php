7766baee9814feb799a96e8c5cc27141c6c51498
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-22 14:31:21 +1200

    Update ldap_to_mysql.php

--- cli/ldap_to_mysql.php
+++ cli/ldap_to_mysql.php
@@ -2,9 +2,10 @@
 
   $root = realpath(dirname(__FILE__));
   
-  //include_once($root."../config/config.php");
+  include_once(realpath($root.'/../config/config.php'));
+  
   echo "\n\r\n\r";
-  echo realpath($root.'/../config/config.php');
+
   echo "\n\r\n\r";
   
 ?>
