7edea47f30ed87bfbbcc82045024bb40aeb9a48f
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-23 09:13:28 +1200

    Update ldap.php

--- lib/ldap.php
+++ lib/ldap.php
@@ -79,7 +79,7 @@ function ldap_get_members($ldap_fqdn,$ldap_port,$ldap_user,$ldap_pass,$search_gr
       echo $member_attr[0]['userprincipalname'][0]."\r\n";
       echo substr(decbin($member_attr[0]['useraccountcontrol'][0]),-2,1)."\r\n";
       echo $member_attr[0]['mail'][0]."\r\n";
-      print_r(array($member_attr[0]['userprincipalname'][0],decbin($member_attr[0]['useraccountcontrol'][0]),$member_attr[0]['mail'][0]));
+      print_r(array($member_attr[0]['userprincipalname'][0],substr(decbin($member_attr[0]['useraccountcontrol'][0]),-2,1),$member_attr[0]['mail'][0]));
       // combine result attributes
       //$member_result = array_merge($member_result,$member_attr);
     }
