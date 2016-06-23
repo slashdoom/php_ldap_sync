306b9d6cb7052e1ac7aaaecec2c7ddd017ff6d72
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-23 12:33:22 +1200

    Update ldap_to_mysql.php

--- cli/ldap_to_mysql.php
+++ cli/ldap_to_mysql.php
@@ -8,10 +8,14 @@
   
   touch(realpath($root.'/../log/_default.log'));
   $logger = new logger(realpath($root.'/../log/_default.log'),$logging_level);
+
+  $ldap_users = array();
+  $mysql_users = array();
   
-  echo "\n\rStarting sync...\n\r";
-  $logger->debug("Starting sync...");
+  echo "\n\rstarting sync...\n\r";
+  $logger->debug("starting sync...");
   
-  print_r(array_column(ldap_get_members($ldap_fqdn,$ldap_port,$ldap_search_user,$ldap_search_pass,"CN=wifi_byod_access,OU=groups,OU=org,DC=ad,DC=slashdoom,DC=lan",$logging_level,realpath($root.'/../log/_ldap.log')),'username'));
+  $ldap_users = array_column(ldap_get_members($ldap_fqdn,$ldap_port,$ldap_search_user,$ldap_search_pass,"CN=wifi_byod_access,OU=groups,OU=org,DC=ad,DC=slashdoom,DC=lan",$logging_level,realpath($root.'/../log/_ldap.log')),'username'));
+  $mysql_users = mysql_get_users($db_host,$db_rw_user,$db_rw_pass,$db_name,$logging_level,realpath($root.'/../log/_ldap.log'))
 
 ?>
