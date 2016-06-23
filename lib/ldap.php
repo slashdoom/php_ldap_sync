5c5ceb9e7ca5da640f062ccceada1558ab5bffb7
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-23 13:45:40 +1200

    Update ldap.php

--- lib/ldap.php
+++ lib/ldap.php
@@ -91,9 +91,9 @@ function ldap_get_members($ldap_fqdn,$ldap_port,$ldap_user,$ldap_pass,$search_gr
     }
     $member_attr = ldap_get_entries($ldap_conn_stat,$member_result_stat);
 
-    // combine result attributes
     // check for disabled account
     if (substr(decbin($member_attr[0]['useraccountcontrol'][0]),-2,1) <> TRUE) {
+      // combine result attributes of enabled accounts
       $member_result[] = array('username'=>$member_attr[0]['userprincipalname'][0],'mail'=>$member_attr[0]['mail'][0]);
     }
   }
