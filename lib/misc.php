d1e872606f171fb6771f113313402ec9da7062bd
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-24 16:37:00 +1200

    Update misc.php

--- lib/misc.php
+++ lib/misc.php
@@ -1,10 +1,20 @@
+<?php
 
+/************************************************************
+* FILENAME:    misc.php
+* DESCRIPTION: simple misc functions
+* 
+* AUTHOR:      Patrick K. Ryon (Slashdoom)
+* LICENSE:     BSD 3-clause (see LICENSE file)
+************************************************************/
 
-function random_str($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+  function random_str($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
     $str = '';
     $max = mb_strlen($keyspace, '8bit') - 1;
     for ($i = 0; $i < $length; ++$i) {
         $str .= $keyspace[random_int(0, $max)];
     }
     return $str;
-}
+  }
+
+?>
