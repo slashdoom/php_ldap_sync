658c52fc1da8b1b4cdc22945b93466a34362331a
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-24 16:49:04 +1200

    Update mysql.php

--- lib/mysql.php
+++ lib/mysql.php
@@ -7,38 +7,46 @@
 * LICENSE:     BSD 3-clause (see LICENSE file)
 ************************************************************/
 
-function mysql_get_users($db_host, $db_user, $db_pass, $db_name,$log_level,$log_file) {
+  function mysql_get_users($db_host, $db_user, $db_pass, $db_name,$log_level,$log_file) {
  
-  $root = realpath(dirname(__FILE__));
+    $root = realpath(dirname(__FILE__));
  
-  require_once(realpath($root.'/../lib/logging.php'));
+    require_once(realpath($root.'/../lib/logging.php'));
 
-  // setup logging
-  touch($log_file);
-  $logger = new logger($log_file,$log_level);
+    // setup logging
+    touch($log_file);
+    $logger = new logger($log_file,$log_level);
   
-  // connect to SQL server
-  $logger->debug("connecting to mysql ".$db_host.",".$db_name);
-  $db_conn_stat = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    // connect to SQL server
+    $logger->debug("connecting to mysql ".$db_host.",".$db_name);
+    $db_conn_stat = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
  
-  // check SQL connection
-  if (mysqli_connect_errno()) {
-    $logger->error("failed to connect to mysql: ".mysqli_connect_error());
-    return false;
+    // check SQL connection
+    if (mysqli_connect_errno()) {
+      $logger->error("failed to connect to mysql: ".mysqli_connect_error());
+      return false;
+    }
+
+    $dc_results = array();
+
+    // define sql and query
+    $db_sql_query="SELECT * FROM radcheck";
+    $db_query=mysqli_query($db_conn_stat,$db_sql_query);
+  
+    // build array from query
+    while($row=mysqli_fetch_array($db_query)) {
+      $dc_results[]=$row['username'];
+      $logger->debug($row['username']." found in db");
+    }
+  
+    return $dc_results;
+
   }
 
-  $dc_results = array();
-
-  // define sql and query
-  $db_sql_query="SELECT * FROM radcheck";
-  $db_query=mysqli_query($db_conn_stat,$db_sql_query);
-  
-  // build array from query
-  while($row=mysqli_fetch_array($db_query)) {
-    $dc_results[]=$row['username'];
-    $logger->debug($row['username']." found in db");
+  function mysql_add_user() {
   }
   
-  return $dc_results;
+  function mysql_remove_user() {
+  }
 
-}
+?>
