aa522fcaf59379620fbe36dec707d86109e71f69
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-26 22:58:46 +1200

    Update mysql.php

--- lib/mysql.php
+++ lib/mysql.php
@@ -7,7 +7,7 @@
 * LICENSE:     BSD 3-clause (see LICENSE file)
 ************************************************************/
 
-  function mysql_get_users($db_host, $db_user, $db_pass, $db_name,$log_level,$log_file) {
+  function mysql_get_users($db_host,$db_user,$db_pass,$db_name,$log_level,$log_file) {
  
     $root = realpath(dirname(__FILE__));
  
@@ -39,6 +39,7 @@
       $logger->debug($row['username']." found in db");
     }
   
+    $db_conn_stat->close();
     return $dc_results;
 
   }
@@ -46,7 +47,42 @@
   function mysql_add_user() {
   }
   
-  function mysql_remove_user() {
+  function mysql_remove_user($db_host,$db_user,$db_pass,$db_name,$username,$log_level,$log_file) {
+    
+    $root = realpath(dirname(__FILE__));
+ 
+    require_once(realpath($root.'/../lib/logging.php'));
+
+    // setup logging
+    touch($log_file);
+    $logger = new logger($log_file,$log_level);
+  
+    // connect to SQL server
+    $logger->debug("connecting to mysql ".$db_host.",".$db_name);
+    $db_conn_stat = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+ 
+    // check SQL connection
+    if (mysqli_connect_errno()) {
+      $logger->error("failed to connect to mysql: ".mysqli_connect_error());
+      return false;
+    }
+    
+    // define sql and query
+    $db_sql_query="DELETE FROM radcheck WHERE username='".$username."'";
+    
+    // delete record from database
+    if ($db_conn_stat->query($db_sql_query) === TRUE) {
+      $logger->debug($username." successfully deleted from mysql database ".$db_name);
+    }
+    else {
+      $logger->error("error deleting mysql record: " . $db_conn_stat->error);
+      $db_conn_stat->close();
+      return false;
+    }
+
+    $db_conn_stat->close();
+    return true;
+    
   }
 
 ?>
