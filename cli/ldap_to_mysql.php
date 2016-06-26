813d2e6e9159d7892342ab382ce33f73f279e86f
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-26 23:12:35 +1200

    Update ldap_to_mysql.php

--- cli/ldap_to_mysql.php
+++ cli/ldap_to_mysql.php
@@ -39,7 +39,8 @@
   echo "\r\nStarting removals...\r\n";
   foreach ($diff_rem as $rem_user) {
     if (mysql_remove_user($db_host,$db_rw_user,$db_rw_pass,$db_name,$rem_user,$log_level,realpath($root.'/../log/_mysql.log')) === TRUE) {
-      echo $rem_user." removed\r\n\";
+      echo "\r\n".$rem_user." removed\r\n\";
     }
   }
+  
 ?>
