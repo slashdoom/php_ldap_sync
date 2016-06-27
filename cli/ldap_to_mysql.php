e378db1ec22ec2474c2694c7c8996a371d8a487a
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-27 15:41:04 +1200

    Update ldap_to_mysql.php

--- cli/ldap_to_mysql.php
+++ cli/ldap_to_mysql.php
@@ -19,14 +19,18 @@
   $logger->debug("starting sync...");
   
   // get all users in group from ldap
+  echo "reading ldap user...\n\r";
   $ldap_users = ldap_get_members(
       $ldap_fqdn,$ldap_port,$ldap_search_user,$ldap_search_pass,$ldap_user_group,$logging_level,realpath($root.'/../log/_ldap.log')
                                 );
   // get all user in mysql db
+  echo "reading mysql user...\n\r";
   $mysql_users = mysql_get_users($db_host,$db_rw_user,$db_rw_pass,$db_name,$logging_level,realpath($root.'/../log/_mysql.log'));
 
   // get users in ldap but not mysql
+  echo "diff ldap against mysql users...\n\r";
   $diff_add = array_diff(array_column($ldap_users,'username'), $mysql_users);
+  echo "diff mysql against ldap users...\n\r";
   // get users in mysql but not ldap
   $diff_rem = array_diff($mysql_users, array_column($ldap_users,'username'));
 
@@ -46,16 +50,18 @@
   foreach ($diff_rem as $rem_user) {
     // logging only
     $logger->debug("starting removals...");
+    echo "removing user ".$rem_user."\r\n";
     $logger->debug("removing user ".$rem_user);
     
     // remove user from mysql db
     mysql_remove_user($db_host,$db_rw_user,$db_rw_pass,$db_name,$rem_user,$logging_level,realpath($root.'/../log/_mysql.log'));
   }
 
-  echo "\r\nstarting adds...\r\n";
+  echo "\r\nstarting any adds...\r\n";
   foreach ($diff_add as $add_user) {
     // logging only
     $logger->debug("starting adds...");
+    echo "adding user ".$add_user."\r\n";
     $logger->debug("adding user ".$add_user);
     
     // generate 'random' password
@@ -68,5 +74,7 @@
     // add user to mysql db
     mysql_add_user($db_host,$db_rw_user,$db_rw_pass,$db_name,$add_user,$add_user_pass,$add_user_mail,$logging_level,realpath($root.'/../log/_mysql.log'));
   }
+  
+  echo "...done.\r\n";
 
 ?>
