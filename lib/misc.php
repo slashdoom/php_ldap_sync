4027514e7f32ebe8ea9ad9ef5f153d953c8c0a0e
Slashdoom <slashdoom@users.noreply.github.com>
2016-06-27 12:24:02 +1200

    Update misc.php

--- lib/misc.php
+++ lib/misc.php
@@ -12,7 +12,7 @@
     $str = '';
     $max = mb_strlen($keyspace, '8bit') - 1;
     for ($i = 0; $i < $length; ++$i) {
-        $str .= $keyspace[random_int(0, $max)];
+        $str .= $keyspace[mt_rand(0, $max)];
     }
     return $str;
   }
