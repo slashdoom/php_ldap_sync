29fec35d127fdefee1d4d2a50f74f038ae1d6187
Slashdoom <slashdoom@users.noreply.github.com>
2016-07-05 09:44:00 +1200

    Update ldap_to_mysql.php

--- cli/ldap_to_mysql.php
+++ cli/ldap_to_mysql.php
@@ -19,25 +19,28 @@
   $logger->debug("starting sync...");
   
   // get all users in group from ldap
-  echo "reading ldap user...\n\r";
+  echo "reading ldap users...\n\r";
+  $logger->debug("reading ldap users...");
   $ldap_users = ldap_get_members(
       $ldap_fqdn,$ldap_port,$ldap_search_user,$ldap_search_pass,$ldap_user_group,$logging_level,realpath($root.'/../log/_ldap.log')
                                 );
   // get all user in mysql db
-  echo "reading mysql user...\n\r";
+  echo "reading mysql users...\n\r";
+  $logger->debug("reading mysql users...");
   $mysql_users = mysql_get_users($db_host,$db_rw_user,$db_rw_pass,$db_name,$logging_level,realpath($root.'/../log/_mysql.log'));
 
   // get users in ldap but not mysql
   echo "diff ldap against mysql users...\n\r";
+  $logger->debug("diff ldap against mysql users...");
   $diff_add = array_diff(array_column($ldap_users,'username'), $mysql_users);
   echo "diff mysql against ldap users...\n\r";
+  $logger->debug("diff mysql against ldap users...");
   // get users in mysql but not ldap
   $diff_rem = array_diff($mysql_users, array_column($ldap_users,'username'));
 
   echo "\r\nstarting removals...\r\n";
+  $logger->debug("starting removals...");
   foreach ($diff_rem as $rem_user) {
-    // logging only
-    $logger->debug("starting removals...");
     echo "removing user ".$rem_user."\r\n";
     $logger->debug("removing user ".$rem_user);
     
@@ -46,9 +49,8 @@
   }
 
   echo "\r\nstarting any adds...\r\n";
+  $logger->debug("starting adds...");
   foreach ($diff_add as $add_user) {
-    // logging only
-    $logger->debug("starting adds...");
     echo "adding user ".$add_user."\r\n";
     $logger->debug("adding user ".$add_user);
     
@@ -64,5 +66,6 @@
   }
   
   echo "...done.\r\n";
+  $logger->debug("...done.");
 
 ?>
